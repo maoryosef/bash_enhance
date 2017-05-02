3fe34cf554f58a40ed27d56583b74c5ab2413a4c
Maor Yosef <maory@wix.com>
2017-05-02 12:17:22 +0300

    eslint apply on bin folder, refactor args parsing in bin, prepare for preview command

--- hless/src/index.js
+++ hless/src/index.js
@@ -1,24 +1,15 @@
 /**
  * Requires rougify to be installed in order to work
  */
-const _ = require('lodash');
 const highlighter = require('./highlighter.js');
 const lessLauncher = require('./lessLauncher.js');
-const args = process.argv.splice(2, 1);
 
-if (_.isEmpty(args)) {
-    console.log('Usage: hless filename[:linenumber]');
-    process.exit(1);
+function openHighlightedLess(filename, lineNumber) {
+    highlighter.highlightFile(filename, lineNumber, content => {
+        lessLauncher.launch(content, lineNumber);
+    });
 }
 
-const filenameParts = args[0].split(':');
-const filename = filenameParts[0];
-let lineNumber = 0;
-
-if (filenameParts.length > 1) {
-    lineNumber = parseInt(filenameParts[1], 10);
-}
-
-highlighter.highlightFile(filename, lineNumber, content => {
-    lessLauncher.launch(content, lineNumber);
-});
+module.exports = {
+    openHighlightedLess
+};
