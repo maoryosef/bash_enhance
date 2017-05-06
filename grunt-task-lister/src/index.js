8e94dc717fb827658d18e6fe3a7d9162a56669a0
Maor Yosef <maory@wix.com>
2017-05-06 15:17:58 +0300

    List grunt tasks nativly

--- grunt-task-lister/src/index.js
+++ grunt-task-lister/src/index.js
@@ -6,23 +6,36 @@ const gruntFileName = `${process.cwd()}/Gruntfile.js`;
 
 if (!fs.existsSync(gruntFileName)) {
     console.error('Unable to locate Gruntfile.js in current directory');
-    process.exit(1);    
+    process.exit(1);
 }
-
-const registeredTasks = [];
-grunt.registerTask = t => {
-    registeredTasks.push(t);
-};
 
 const gruntFile = require(gruntFileName);
 
 gruntFile(grunt);
 
-_(registeredTasks)
-    .uniq()
+// Initialize task system so that the tasks can be listed.
+grunt.task.init([], {help: true});
+let longestTaskName = 20;
+
+// Build object of tasks by info (where they were loaded from).
+_(grunt.task._tasks)
+    .keys()
+    .forEach(name => {
+        if (name.length > longestTaskName) {
+            longestTaskName = name.length;
+        }
+    })
     .sort()
+    .map(key => grunt.task._tasks[key])
     .forEach(task => {
-        console.log(task);
+        let info = task.info;
+        if (task.multi) {
+            info += ' *';
+        }
+
+        const spaceLength = longestTaskName + 1 - task.name.length;
+
+        console.log(`${task.name}${' '.repeat(spaceLength)} -    ${info}`);
     });
 
 process.exit();
