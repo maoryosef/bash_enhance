bab16bf8787c85326bf08ec2adcb0e3fbf48bd7e
Maor Yosef <maory@wix.com>
2017-05-06 12:38:20 +0300

    Exit if no gruntfile exists

--- grunt-task-lister/src/index.js
+++ grunt-task-lister/src/index.js
@@ -1,12 +1,20 @@
 const grunt = require('grunt');
 const _ = require('lodash');
+const fs = require('fs');
+
+const gruntFileName = `${process.cwd()}/Gruntfile.js`;
+
+if (!fs.existsSync(gruntFileName)) {
+    console.error('Unable to locate Gruntfile.js in current directory');
+    process.exit(1);    
+}
 
 const registeredTasks = [];
 grunt.registerTask = t => {
     registeredTasks.push(t);
 };
 
-const gruntFile = require(`${process.cwd()}/Gruntfile.js`);
+const gruntFile = require(gruntFileName);
 
 gruntFile(grunt);
 
